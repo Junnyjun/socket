6131ce7ba67f6a672e48c295f8f7a2766051621a
Junnyjun <chbe5082@naver.com>
2022-08-21 11:13:13 +0900

    infra

--- infra/run.sh
+++ infra/run.sh
@@ -1,16 +1,14 @@
 CONTAINER_NAME=socket_container
 IMAGE_NAME=socket_image
+REGISTRY_HOST=144.24.87.198:5000
 PORT=8088
+
+NUMBER=$1
 
 sudo docker stop $CONTAINER_NAME
 sudo docker rm $CONTAINER_NAME
 sudo docker rmi $IMAGE_NAME
 
 sudo docker build \
--t $IMAGE_NAME \
+--tag $REGISTRY_HOST/$IMAGE_NAME:"$NUMBER" \
 .
-
-sudo docker run \
--p $PORT:8080 \
--name $CONTAINER_NAME \
-$IMAGE_NAME
