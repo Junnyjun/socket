a545e8188e48b3b7d3e79723882dbf63bdeda9cd
Junnyjun <chbe5082@naver.com>
2022-08-21 11:40:49 +0900

    infra

--- infra/run.sh
+++ infra/run.sh
@@ -1,7 +1,7 @@
 CONTAINER_NAME=socket_container
 IMAGE_NAME=socket_image
 PORT=8088
-
+BUILD_PATH=$1
 
 sudo docker stop $CONTAINER_NAME
 sudo docker rm $CONTAINER_NAME
@@ -9,7 +9,7 @@ sudo docker rmi $IMAGE_NAME
 
 sudo docker build \
 -t $IMAGE_NAME \
-.
+$BUILD_PATH
 
 sudo docker run \
 -p $PORT:8080 \
